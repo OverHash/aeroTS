31c75d6ee30dab1d3fb0c1a562275edd62aba25f
OverHash <46231745+OverHash@users.noreply.github.com>
2020-07-14 11:49:52 +1200

    Remove ListenerList from type defs

--- src/GlobalRegistry.d.ts
+++ src/GlobalRegistry.d.ts
@@ -6,7 +6,6 @@ import Base64 from "Shared/Base64";
 import TableUtil from "Shared/TableUtil";
 import NumberUtil from "Shared/NumberUtil";
 import VectorUtil from "Shared/VectorUtil";
-import ListenerList from "Shared/ListenerList";
 
 // import modules here, e.g.
 
@@ -22,8 +21,8 @@ import ListenerList from "Shared/ListenerList";
 declare global {
 	// put all shared stuff here
 	interface shared {
-		// module: moduleDefinition
-		// e.g. module2: module2;
+		// module: typeof moduleImport
+		// e.g. module2: typeof module2;
 		Date: typeof Date;
 		Maid: typeof Maid;
 		Event: typeof Event;
@@ -32,33 +31,32 @@ declare global {
 		TableUtil: typeof TableUtil;
 		NumberUtil: typeof NumberUtil;
 		VectorUtil: typeof VectorUtil;
-		ListenerList: typeof ListenerList;
 	}
 
 	// put all server stuff here
 	interface server {
 		services: {
-			// service: serviceDefinition
-			// e.g. myService: myService;
+			// service: serviceImport
+			// e.g. myService: typeof myService;
 		},
 
 
 		modules: {
-			// module: moduleDefinition
-			// e.g. myServerModule: myServerModule
+			// module: moduleImport
+			// e.g. myServerModule: typeof myServerModule
 		}
 	}
 
 	// put all client stuff here
 	interface client {
 		controllers: {
-			// controller: controllerDefinition
-			// e.g. myController: myController
+			// controller: controllerImport
+			// e.g. myController: typeof myController
 		},
 
 		modules: {
-			// module: moduleDefinition
-			// e.g. myClientModule: myClientModule
+			// module: moduleImport
+			// e.g. myClientModule: typeof myClientModule
 		}
 	}
 }
